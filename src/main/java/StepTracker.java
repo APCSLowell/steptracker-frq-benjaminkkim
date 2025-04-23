68b2b79fa066ff68a62aeaa965650b4e10831be5
benjaminkkim <bekim@s.sfusd.edu>
2025-04-23 11:09:26 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
+int dailySteps = 0;
+int totalDays = 0;
 public class StepTracker {
  int steps;
  public StepTracker(int numSteps) {
   numSteps = steps;
  }
  public void addDailySteps(int n) {
-  int dailySteps = 0;
   n += steps;
   dailySteps++;
  }
  public int activeDays() {
-  int totalDays = 0;
   for(int i = 0; i < dailySteps; i++) {
    if(steps >= 10000) {
     totalDays++;
